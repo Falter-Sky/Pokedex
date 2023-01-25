fc167787b82dd94ef4919ac1c8ea41a927f9cd39
Adalan <sunymon@protonmail.com>
2023-01-24 20:44:39 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { page } from '$app/stores';
+	import { generations } from './generatrations';
+	import { goto } from '$app/navigation';
+	import Monster from './Monster.svelte';
+
+	export let data: PageData;
+
+	$: monsterId = $page.url.searchParams.get('monsterId') || '';
+	$: monster = data.monsters.find((monster) => monster.id === monsterId);
+	$: monsterId2 = $page.url.searchParams.get('monsterId2') || '';
+	$: monster2 = data.monsters.find((monster) => monster.id === monsterId2);
+
+	const updateSearchParams = (key: string, value: string) => {
+		const searchParams = new URLSearchParams($page.url.searchParams);
+		searchParams.set(key, value);
+		goto(`?${searchParams.toString()}`);
+	};
+</script>
+
+{#if monster}
+	<Monster {monster} {updateSearchParams} />
+{/if}
+{#if monster2}
+	<Monster monster={monster2} {updateSearchParams} />
+{/if}
+
+<div class="generations">
+	{#each generations as generation (generation.id)}
+		<div class="generation">{generation.main_region}</div>
+	{/each}
+</div>
+
+<div class="monsters">
+	{#each data.monsters as monster (monster.id)}
+		<Monster {monster} {updateSearchParams} isInteractive={true} />
+	{/each}
+</div>
+
+<style>
+	.generations {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+	.generation {
+		margin: 5px;
+		padding: 5px 10px;
+		border: 1px solid black;
+		background-color: #f9f9f9;
+		color: #333;
+	}
+	.generation:hover {
+		background-color: #eee;
+	}
+	.monsters {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+</style>
